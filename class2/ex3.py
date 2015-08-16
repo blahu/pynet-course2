dc9b59bb7affc0d054ee4172403f78774571e480
blahu <blahu77+pynet@gmail.com>
2015-08-16 14:13:10 -0700

    class2 exercise 3, done

---
+++ class2/ex3.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+"""
+3. telnetlib (optional - challenge question)
+
+    Convert the code that I wrote here to a class-based solution (i.e. convert
+over from functions to a class with methods).
+"""
+
+'''
+Write a script that connects to the lab pynet-rtr1, logins, and executes the
+'show ip int brief' command.
+'''
+
+import telnetlib
+import time
+import socket
+import sys
+import getpass
+
+class Telneto :
+
+    TELNET_PORT = 23
+    TELNET_TIMEOUT = 6
+
+    def __init__ (self, **kwargs):
+
+        self.ip_address = kwargs.get('ip_address')
+        self.username = kwargs.get('username')
+        self.password = kwargs.get('password')
+
+        self.remote_conn = self.telnet_connect()
+
+        self.login()
+        self.disable_paging()
+
+    def telnet_connect(self):
+        '''
+        Establish telnet connection
+        '''
+        try:
+            return telnetlib.Telnet(self.ip_address, self.TELNET_PORT, self.TELNET_TIMEOUT)
+        except socket.timeout:
+            sys.exit("Connection timed-out")
+    
+    def login(self):
+        '''
+        Login to network device
+        '''
+        output = self.remote_conn.read_until("sername:", self.TELNET_TIMEOUT)
+        self.remote_conn.write(self.username + '\n')
+        output += self.remote_conn.read_until("ssword:", self.TELNET_TIMEOUT)
+        self.remote_conn.write(self.password + '\n')
+        return output
+    
+
+    def send_command(self, cmd):
+        '''
+        Send a command down the telnet channel
+    
+        Return the response
+        '''
+        cmd = cmd.rstrip()
+        self.remote_conn.write(cmd + '\n')
+        time.sleep(1)
+        return self.remote_conn.read_very_eager()
+    
+    def disable_paging(self, paging_cmd='terminal length 0'):
+        '''
+        Disable the paging of output (i.e. --More--)
+        '''
+        return self.send_command(paging_cmd)
+    
+    def close(self):
+        '''
+        Close telnet connection
+        '''
+        return self.remote_conn.close()
+    
+def main():
+    '''
+    Write a script that connects to the lab pynet-rtr1, logins, and executes the
+    'show ip int brief' command.
+    '''
+    ip_addr = raw_input("IP address: ")
+    ip_addr = ip_addr.strip()
+    username = 'pyclass'
+    password = getpass.getpass()
+    kwargs = {'ip_address':ip_addr, 'username':username, 'password':password} 
+    remote_conn = Telneto(**kwargs)
+
+    output = remote_conn.send_command('show ip int brief')
+
+    print "\n\n"
+    print output
+    print "\n\n"
+
+    remote_conn.close()
+
+if __name__ == "__main__":
+    main()
